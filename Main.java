fb6a73775bccfa77f7dad754f9a3c45a86324462
Ivan <ikduong100@student.hbuhsd.edu>
2024-11-18 19:09:10 +0000

    dos

--- Main.java
+++ Main.java
@@ -5,6 +5,7 @@ public class Main
   public static void main(String[] args)
   {
     printNTimes("code", 3);
+    printNums();
   }
 
   public static void printNTimes(String word, int N)
@@ -20,7 +21,13 @@ public class Main
 
   public static void printNums()
   {
-    
+    for (int i = 10; i > 0; i--){
+      for ( int a = 0; a < i; a++){
+        System.out.print(i + " ");
+      }
+      System.out.println();
+    }
+
   }
 
   public static void uprightNumberTriangle(int N)
